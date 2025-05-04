c51a0029bbebedb50cb7d00b8526c476f66caac5
Sebastian Lemus Cadena <sebastian.slc9@gmail.com>
2025-05-04 13:44:45 -0500

    Simplifies ExprPrinterTests

--- modules/proust/src/test/scala/dev/proust/printer/ExprPrinterTests.scala
+++ modules/proust/src/test/scala/dev/proust/printer/ExprPrinterTests.scala
@@ -1,56 +1,31 @@
 package dev.proust.printer
 
-import dev.proust.lang.Expr
 import dev.proust.lang.GoalNumber
-import dev.proust.lang.Identifier
+import dev.proust.parser.ExprParsers
 import weaver.FunSuite
 
 object ExprPrinterTests extends FunSuite:
 
   test("prints variables"):
-    val variable: Expr.Var = Expr.Var(Identifier("xsd21s"))
-    expect.same(
-      expected = variable.name,
-      found = ExprPrinter.print(variable)
-    )
+    val program = "xsd21s"
+    val expr    = ExprParsers.annotatedExpr.parseAll(program).toOption.get
+    expect.same(expected = program, found = ExprPrinter.print(expr))
 
   test("prints functions"):
-    val fun = Expr.Lambda(
-      Identifier("x"),
-      Expr.Lambda(Identifier("y"), Expr.Var(Identifier("x")))
-    )
-
-    expect.same(
-      expected = "\\x -> \\y -> x",
-      found = ExprPrinter.print(fun)
-    )
+    val program = "\\x -> \\y -> x"
+    val expr    = ExprParsers.annotatedExpr.parseAll(program).toOption.get
+    expect.same(expected = program, found = ExprPrinter.print(expr))
 
   test("prints function applications"):
-    val expr = Expr.Apply(
-      Expr.Apply(
-        Expr.Apply(
-          Expr.Apply(
-            Expr.Var(Identifier("f")),
-            Expr.Lambda(Identifier("x"), Expr.Var(Identifier("x")))
-          ),
-          Expr.Var(Identifier("y"))
-        ),
-        Expr.Apply(Expr.Var(Identifier("g")), Expr.Var(Identifier("z")))
-      ),
-      Expr.Var(Identifier("w"))
-    )
-
-    expect.same(
-      expected = "f (\\x -> x) y (g z) w",
-      found = ExprPrinter.print(expr)
-    )
+    val program = "f (\\x -> x) y (g z) w"
+    val expr    = ExprParsers.annotatedExpr.parseAll(program).toOption.get
+    expect.same(expected = program, found = ExprPrinter.print(expr))
 
   test("prints holes"):
-    val expr = Expr.Lambda(Identifier("x"), Expr.Hole(GoalNumber(10)))
+    val program = "\\x y z -> f (\\x -> ?) ? (\\a b -> b ?) ?"
+    val expr    = ExprParsers.annotatedExpr.parseAll(program).toOption.get
 
     expect.same(
-      expected = "\\x -> 10?",
-      found = ExprPrinter.print(expr)
+      expected = "\\x -> \\y -> \\z -> f (\\x -> 0?) 1? (\\a -> \\b -> b 2?) 3?",
+      found = ExprPrinter.print(expr.numberGoals.runA(GoalNumber(0)).value)
     )
-
-end ExprPrinterTests
