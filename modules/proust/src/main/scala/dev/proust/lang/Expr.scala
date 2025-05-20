b10134059d92bba71aef9b6f110efa6dd1e293a9
Sebastián Lemus <sebastian.slc9@gmail.com>
2025-05-19 22:06:33 -0500

    DynamicExpr  (#6)
    * Add DynamicExpr
    
    * Add DynamicExprTests

--- modules/proust/src/main/scala/dev/proust/lang/Expr.scala
+++ modules/proust/src/main/scala/dev/proust/lang/Expr.scala
@@ -1,6 +1,8 @@
 package dev.proust.lang
 
 import cats.data.State
+import cats.derived.semiauto
+import cats.kernel.Eq
 
 enum Expr:
   self =>
@@ -18,37 +20,28 @@ enum Expr:
   /**
     * Traverse the Expr tree and assign increasing integers to each of the Holes within it
     */
-  def numberGoals: State[GoalNumber, Expr] = self match
+  def assignGoals: State[GoalNumber, Expr] = self match
     case expr: Var => State.pure(expr)
 
     case Hole(goal) => State(n => (n.incr, Hole(n)))
 
     case Lambda(x, body) =>
-      body.numberGoals.map(body => Lambda(x, body))
+      body.assignGoals.map(body => Lambda(x, body))
 
     case Apply(f, a) =>
       for
-        nf <- f.numberGoals
-        na <- a.numberGoals
+        nf <- f.assignGoals
+        na <- a.assignGoals
       yield Apply(nf, na)
 
     case Annotate(expr, _type) =>
-      expr.numberGoals.map(expr => Annotate(expr, _type))
+      expr.assignGoals.map(expr => Annotate(expr, _type))
 
-  /**
-    * Fills the hole identified by the given [[goal]] number, using the given [[content]] expression
-    */
-  def fillGoal(goal: GoalNumber, content: Expr): Expr = self match
-    case expr: Var             => expr
-    case Hole(n) if goal == n  => content
-    case expr: Hole            => expr
-    case Lambda(x, body)       => Lambda(x, body.fillGoal(goal, content))
-    case Apply(f, a)           => Apply(f.fillGoal(goal, content), a.fillGoal(goal, content))
-    case Annotate(expr, _type) => Annotate(expr.fillGoal(goal, content), _type)
+object Expr:
 
-end Expr
-
-object Expr
+  given Eq[Expr] =
+    given Eq[GoalNumber] = Eq.instance((_, _) => true)
+    semiauto.eq
 
 enum TypeExpr:
   case Var(name: Identifier)
@@ -57,3 +50,7 @@ enum TypeExpr:
   def isRecursive: Boolean = this match
     case _: Function => true
     case _           => false
+
+object TypeExpr:
+
+  given Eq[TypeExpr] = semiauto.eq
---
+++ modules/proust/src/main/scala/dev/proust/printer/DynamicExprPrinter.scala
@@ -0,0 +1,32 @@
+package dev.proust.printer
+
+import cats.Show
+import dev.proust.lang.DynamicExpr
+import dev.proust.lang.Expr
+
+object DynamicExprPrinter extends CorePrinters:
+
+  def print(dyn: DynamicExpr): String =
+
+    def printExpr(expr: Expr): String =
+      expr match
+        case Expr.Var(name) =>
+          name
+
+        case Expr.Hole(goal) =>
+          dyn.getGoalExpr(goal).fold(s"$goal?") { expr =>
+            printExpr(expr).inParensIf(expr.isRecursive)
+          }
+
+        case Expr.Lambda(x, body) =>
+          s"\\$x -> ${printExpr(body)}"
+
+        case Expr.Apply(f, a) =>
+          s"${printExpr(f)} ${printExpr(a).inParensIf(a.isRecursive)}"
+
+        case Expr.Annotate(x, t) =>
+          s"${printExpr(x)} : ${TypeExprPrinter.print(t)}"
+
+    printExpr(dyn.expr)
+
+  given Show[DynamicExpr] = Show.show(print)
